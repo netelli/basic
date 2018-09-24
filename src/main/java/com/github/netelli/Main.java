8123b5bd5a8828cd78ecea562c059aa9d99a2ebb
nataliiaku <natalli.dev@gmail.com>
2018-09-24 15:07:55 +0300

    Initial commit.

---
+++ src/main/java/com/github/netelli/Main.java
@@ -0,0 +1,17 @@
+package com.github.netelli;
+
+public class Main {
+    static int a = 1111;
+    static
+    {
+        a = a-- - --a;
+    }
+
+    {
+        a = a++ + ++a;
+    }
+
+    public static void main(String[] args)  {
+        System.out.println(a);
+    }
+}
